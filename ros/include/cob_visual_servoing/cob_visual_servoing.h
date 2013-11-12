f736d8484fa87929ec25ccf948f58bdf555751e3
ipa-fxm <felix.messmer@ipa.fhg.de>
2013-11-12 19:10:45 +0100

    first version of position-controlled arm-monitoring

--- ros/include/cob_visual_servoing/cob_visual_servoing.h
+++ ros/include/cob_visual_servoing/cob_visual_servoing.h
@@ -30,11 +30,52 @@
 
 #include <ros/ros.h>
 
+#include <std_msgs/Float64.h>
+#include <sensor_msgs/JointState.h>
+#include <cob_srvs/Trigger.h>
+#include <controller_manager_msgs/SwitchController.h>
+
+#include <kdl_parser/kdl_parser.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/chainiksolvervel_pinv.hpp>
+#include <kdl/chainiksolverpos_nr.hpp>
+#include <kdl/jntarray.hpp>
+#include <kdl/frames.hpp>
 
 class CobVisualServoing
 {
 private:
 	ros::NodeHandle nh_;
+	
+	ros::ServiceClient switch_controller_client;
+	
+	ros::ServiceServer serv_start;
+	ros::ServiceServer serv_stop;
+	
+	ros::Subscriber js_sub;
+	
+	ros::Publisher torso_lower_pub;
+	ros::Publisher torso_pan_pub;
+	ros::Publisher torso_upper_pub;
+	
+	bool b_servoing;
+	unsigned int throttle_;
+	
+	KDL::Chain chain_arm_;
+	KDL::Chain chain_lookat_;
+	
+	KDL::ChainFkSolverPos_recursive* p_fksolver_pos_arm_;
+	KDL::ChainIkSolverVel_pinv* p_iksolver_vel_arm_;
+	KDL::ChainIkSolverPos_NR* p_iksolver_pos_arm_;
+	KDL::ChainFkSolverPos_recursive* p_fksolver_pos_lookat_;
+	KDL::ChainIkSolverVel_pinv* p_iksolver_vel_lookat_;
+	KDL::ChainIkSolverPos_NR* p_iksolver_pos_lookat_;
+	
+	
+	
+	//helper functions
+	bool parseJointStates(std::vector<std::string> names, std::vector<double> positions, std::vector<double> velocities, KDL::JntArray& q, KDL::JntArray& q_dot);
+	
 	
 public:
 	CobVisualServoing() {;}
@@ -42,6 +83,12 @@ public:
 	
 	void initialize();
 	void run();
+	
+	
+	bool start_cb(cob_srvs::Trigger::Request& request, cob_srvs::Trigger::Response& response);
+	bool stop_cb(cob_srvs::Trigger::Request& request, cob_srvs::Trigger::Response& response);
+	
+	void jointstate_cb(const sensor_msgs::JointState::ConstPtr& msg);
 
 };
 #endif
